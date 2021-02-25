2f9d4fa1d23e47915d05a1b1b5923c78c6c423c5
JSoto <sotojosh96@gmail.com>
2021-02-25 11:42:39 -0800

    added start button to hmtl and adjusted css to account for new styles related to start button layout. also added more javascript variables

--- development/javascript/script.js
+++ development/javascript/script.js
@@ -1,6 +1,19 @@
 // variable declarations
 var quizQuestion = document.querySelector(".quiz-question")
 console.log(quizQuestion)
+var quizChoiceFirst = document.querySelector(".mc-1-choice")
+var quizChoiceSecond = document.querySelector(".mc-2-choice")
+var quizChoiceThird = document.querySelector(".mc-3-choice")
+var quizChoiceFourth = document.querySelector(".mc-4-choice")
+console.log(quizChoiceFirst)
+console.log(quizChoiceSecond)
+console.log(quizChoiceThird)
+console.log(quizChoiceFourth)
+
+quizChoiceFirst.textContent = "Multiple Choice 1"
+quizChoiceSecond.textContent = "Multiple Choice 2"
+quizChoiceThird.textContent = "Multiple Choice 3"
+quizChoiceFourth.textContent = "Multiple Choice 4"
 
 // quizQuestion.textContent = "This is the new queston"
 // console.log(quizQuestion.textContent)
