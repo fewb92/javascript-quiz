93fb72ac4c41537fe2a2528fd4c0986979b0c2b1
JSoto <sotojosh96@gmail.com>
2021-02-25 09:53:19 -0800

    added a new html div to incorporate leaderboards, sudo coded JS and tweaked CSS so that leaderboard and main game appear side by side

---
+++ development/javascript/script.js
@@ -0,0 +1,16 @@
+// variable declarations
+var quizQuestion = document.querySelector(".quiz-question")
+console.log(quizQuestion)
+
+// quizQuestion.textContent = "This is the new queston"
+// console.log(quizQuestion.textContent)
+
+// start game function
+    // timer function
+    // [quesiton loop function - needs own array
+    // answer loop function - needs own array]
+    // evaluate correct user submited answer
+    // if question answer = false subtract time from timer
+    // game over at timer reach zero OR all questions answered game over 
+    // when game over add initials to save high score
+    // write high score to leaderboard
