db089430d158dba951cdca5ade47c969b27a686e
fewb92 <sotojosh96@gmail.com>
2021-08-05 21:53:39 -0400

    added show and hide to css, fixed index.js

--- development/javascript/script.js
+++ development/javascript/script.js
@@ -1,233 +1,118 @@
-// variable declarations
-var quizQuestion = document.querySelector(".quiz-question")
-console.log(quizQuestion)
-var quizChoices = document.querySelector(".quiz-choices")
-var quizChoiceFirst = document.querySelector(".mc-1-choice")
-var quizChoiceSecond = document.querySelector(".mc-2-choice")
-var quizChoiceThird = document.querySelector(".mc-3-choice")
-var quizChoiceFourth = document.querySelector(".mc-4-choice")
-// var quizChoice = document.querySelectorAll(".mc-choice")
-var timerSeconds = document.querySelector(".timer-seconds")
+// Assigning variables to HTML elements
+var startButtonEl = document.getElementById("start");
+var questionEl = document.getElementById("question");
+var currentScoreEl = document.getElementById("score");
+var answersEl = document.getElementById("answers");
+var timerEl = document.getElementById("countTime");
+var correctText = document.getElementById("correctText");
+var wrongText = document.getElementById("wrongText");
+var highScoreEl = document.getElementById("highScore");
+var body = document.getElementById("body");
+var playerName = document.getElementById("playerName");
 
-var timerCount = 60;
+// Defining global variables
+var score = 0;
+var highScore = 0;
+var leaderboard = JSON.parse(localStorage.getItem("leaderboard")) || [];
+var timer;
+var countTime;
+var questionOrder = 0;
+var ask;
+var buttonOne;
+var buttonTwo;
+var buttonThree;
+var buttonFour;
+var answers;
+var question;
+var playButton;
 
-// console.log(quizChoiceFirst)
-// console.log(quizChoiceSecond)
-// console.log(quizChoiceThird)
-// console.log(quizChoiceFourth)
-var btnStart = document.querySelector(".start-button")
+// Push start button to begin game
+startButtonEl.addEventListener("click", beginGame);
 
-var allQuestions = [
+//List of questions
+var questions = [
     {
-        Question: 'What is a boolean?',
-        Choices: ['A true or false value', 'A number', 'Dead (leftover) code that creates bugs', 'An undefined value'],
-        Correct: 'A true or false value'
+        prompt: 'What is a boolean?',
+        choices: ['A true or false value', 'A number', 'Dead (leftover) code that creates bugs', 'An undefined value'],
+        answer: '0'
     },
     {
-        Question: 'What is a method in JavaScript?',
-        Choices: ['A tool that enables arithmetic', 'A popular solution to a known issue', 'A predefined function within Javascript', 'A piece of code that makes HTML'],
-        Correct: 'A predefined function within Javascript'
+        prompt: 'What is a method in JavaScript?',
+        choices: ['A tool that enables arithmetic', 'A popular solution to a known issue', 'A predefined function within Javascript', 'A piece of code that makes HTML'],
+        answer: '2'
     },
     {
-        Question: 'What is the correct syntax for linking to an external file called script.js?',
-        Choices: ['<script = script.js/>', '<script src="script.js">', '<script src="script.js/>', '<script: script.js>'],
-        Correct: '<script src="script.js>'
+        prompt: 'What is the correct syntax for linking to an external file called script.js?',
+        choices: ['<script = script.js/>', '<script src="script.js">', '<script src="script.js/>', '<script: script.js>'],
+        answer: '1'
     },
     {
-        Question: 'What functionality can be built with Javascript?',
-        Choices: ['A rock, paper, scissors game', 'An app that keeps track of a personal calendar', 'An app that displays weather data', 'All of the above'],
-        Correct: 'A true or false value'
+        prompt: 'What functionality can be built with Javascript?',
+        choices: ['A rock, paper, scissors game', 'An app that keeps track of a personal calendar', 'An app that displays weather data', 'All of the above'],
+        answer: '3'
     },
     {
-        Question: 'What does const represent in Javascript',
-        Choices: ['A variable that can be updated', 'A variable that is always false', 'A variable that is always a number', 'A variable whose value cannot be changed'],
-        Correct: 'A number'
+        prompt: 'What does const represent in Javascript',
+        choices: ['A variable that can be updated', 'A variable that is always false', 'A variable that is always a number', 'A variable whose value cannot be changed'],
+        answer: '3'
     }
 ]
 
-// allQuestions[i].Question
-// allQuestions[i].Choices[0]
-// allQuestions[i].Choices[1]
-// allQuestions[i].Choices[2]
-// allQuestions[i].Choices[3]
-// allQuestions[i].Correct
+// Initialize
+init();
 
-var count = 0
-
-// for (i = 0; i < 5; i++) {
-//     var currentQuestion = allQuestions[i].Question
-//     console.log(currentQuestion)
-
-//     if (currentQuestion === allQuestions[0].Question) {
-//         quizQuestion.innerHTML = currentQuestion
-//         quizChoiceFirst.innerHTML = allQuestions[0].Choices[0]
-//         quizChoiceSecond.innerHTML = allQuestions[0].Choices[1]
-//         quizChoiceThird.innerHTML = allQuestions[0].Choices[2]
-//         quizChoiceFourth.innerHTML = allQuestions[0].Choices[3]
-//     } else if (currentQuestion === allQuestions[1].Question) {
-//         quizQuestion.innerHTML = currentQuestion
-//         quizChoiceFirst.innerHTML = allQuestions[1].Choices[0]
-//         quizChoiceSecond.innerHTML = allQuestions[1].Choices[1]
-//         quizChoiceThird.innerHTML = allQuestions[1].Choices[2]
-//         quizChoiceFourth.innerHTML = allQuestions[1].Choices[3]
-//     } else if (currentQuestion === allQuestions[2].Question) {
-//         quizQuestion.innerHTML = currentQuestion
-//         quizChoiceFirst.innerHTML = allQuestions[2].Choices[0]
-//         quizChoiceSecond.innerHTML = allQuestions[2].Choices[1]
-//         quizChoiceThird.innerHTML = allQuestions[2].Choices[2]
-//         quizChoiceFourth.innerHTML = allQuestions[2].Choices[3]
-//     } else if (currentQuestion === allQuestions[3].Question) {
-//         quizQuestion.innerHTML = currentQuestion
-//         quizChoiceFirst.innerHTML = allQuestions[3].Choices[0]
-//         quizChoiceSecond.innerHTML = allQuestions[3].Choices[1]
-//         quizChoiceThird.innerHTML = allQuestions[3].Choices[2]
-//         quizChoiceFourth.innerHTML = allQuestions[3].Choices[3]
-//     } else {
-//         quizQuestion.innerHTML = currentQuestion
-//         quizChoiceFirst.innerHTML = allQuestions[4].Choices[0]
-//         quizChoiceSecond.innerHTML = allQuestions[4].Choices[1]
-//         quizChoiceThird.innerHTML = allQuestions[4].Choices[2]
-//         quizChoiceFourth.innerHTML = allQuestions[4].Choices[3]
-//     }
-// }
-
-
-// console.log(allQuestions[0].Choices[2])
-
-
-// quizChoiceFirst.textContent = "Multiple Choice 1"
-// quizChoiceSecond.textContent = "Multiple Choice 2"
-// quizChoiceThird.textContent = "Multiple Choice 3"
-// quizChoiceFourth.textContent = "Multiple Choice 4"
-
-// quizQuestion.textContent = "This is the new queston"
-// console.log(quizQuestion.textContent)
-
-function startTimer() {
-    timer = setInterval(function() {
-        timerCount--
-        timerSeconds.innerHTML = timerCount;
-        // console.log(timerCount)
-        // make sure time doesn't run after hitting zero
-        if (timerCount === 0){
-        clearInterval(timer);
-        }
-    }, 1000)
+function init() {
+    returnHighscore();
 }
 
-// function incrementCount() {
-
-// }
-
-
-
-
-btnStart.onclick = function () {
-    startTimer()
-    console.log(count)
-    btnStart.style.display = 'none'
-    showQuestion()
-    storeChoice()
-}
-
-function showQuestion() {
-    console.log(count)
-    var currentQuestion = allQuestions[count].Question
-    quizQuestion.textContent = currentQuestion
-    quizChoiceFirst.textContent = allQuestions[count].Choices[0]
-    quizChoiceSecond.textContent = allQuestions[count].Choices[1]
-    quizChoiceThird.textContent = allQuestions[count].Choices[2]
-    quizChoiceFourth.textContent = allQuestions[count].Choices[3]
-}
-
-let html = '<button>Next</button>'
-
-
-const storeChoice = function () {
-quizChoiceFirst.addEventListener('click', event => {
-    var choice = event.target.textContent
-    console.log(choice)
-    if (allQuestions[count].Correct === choice) {
-        count++
-        console.log(timerCount)
-        console.log(count)
-        console.log('right answer')
-
-        let nextBtn = document.createElement('button')
-        nextBtn.innerHTML('<button>Next</button>')
-        nextBtn.style.display = 'block'
-        nextBtn.classList.add('start-button')
-        
-    }
-})
-quizChoiceSecond.addEventListener('click', event => {
-    var choice = event.target.textContent
-    console.log(choice)
-    if (allQuestions[count].Correct === choice) {
-
-        count++
-        console.log(count)
-        console.log('right answer')
-        if (count < allQuestions.length) {
-            const nextBtn = document.createElement('div')
-            nextBtn.classList.add('start-button')
+// Returns the current highscore
+function returnHighscore() {
+    if (JSON.parse(localStorage.getItem("leaderboard"))) {
+        leaderboard = JSON.parse(localStorage.getItem("leaderboard"));
+        leaderboard.sort((a, b) => b.score - a.score);
+        for (var i = 0; i < 1; i++) {
+            highScoreEl.textContent = leaderboard[i].name + " : " + leaderboard[i].score;
         }
-    }
-})
-quizChoiceThird.addEventListener('click', event => {
-    var choice = event.target.textContent
-    console.log(choice)
-    if (allQuestions[count].Correct === choice) {
-        count++
-        console.log(count)
-        console.log('right answer')
-        if (count < allQuestions.length) {
-            const nextBtn = document.createElement('div')
-            nextBtn.classList.add('start-button')
-        }
-    }
-})
-quizChoiceFourth.addEventListener('click', event => {
-    var choice = event.target.textContent
-    console.log(choice)
-    if (allQuestions[count].Correct === choice) {
-        count++
-        console.log(count)
-        console.log('right answer')
-        if (count < allQuestions.length) {
-            const nextBtn = document.createElement('div')
-            nextBtn.classList.add('start-button')
-        }
-    }
-
-    
-})
-}
-
-function answerCheck() {
-    if (this.value == question.answer) {
-        correctText.setAttribute("id", "correctText.show");
-        score++;
-        currentScoreEl.textContent = score;
-        questionOrder++;
-        questionEl.removeChild(ask);
-        answersEl.removeChild(buttonOne);
-        answersEl.removeChild(buttonTwo);
-        answersEl.removeChild(buttonThree);
-        answersEl.removeChild(buttonFour);
-        if (questionOrder < questions.length){
-            cueQuestion();
-        }
-    } else {
-        wrongText.setAttribute("id", "wrongText.show");
-        countTime -= 3;
-        setTimeout(function() { wrongText.setAttribute("id", "wrongText"); }, 500);
     }
 };
 
+// Function to begin the game
+function beginGame() {
+    startButtonEl.disabled = true;
+    beginTimer();
+    cueQuestion();
+};
+
+// Starts the timer and sets the trigger for the end of the game
+function beginTimer() {
+    startButtonEl.setAttribute("class", "d-none");
+    countTime = 50;
+    timerEl.textContent = countTime;
+    timer = setInterval(function() {
+      countTime--;
+      timerEl.textContent = countTime;
+      // If time is out, clear out the question and choices and end the game
+      if (countTime <= 0 || questionOrder == questions.length) {
+        correctText.setAttribute("id", "correctText");
+        if (questionOrder < questions.length) {
+            questionEl.removeChild(ask);
+            target = answersEl;
+            while (target.firstChild) {
+                target.removeChild(target.firstChild);
+            }
+        }
+        clearInterval(timer);
+        countTime = 0;
+        timerEl.textContent = countTime;
+        endGame();
+      }
+    }, 1000);
+};
+
+// Ask the questions
 function cueQuestion() {
     setTimeout(function() { correctText.setAttribute("id", "correctText"); },1500);
-    question = allQuestions[questionOrder];
+    question = questions[questionOrder];
     ask = document.createElement('span');
     ask.setAttribute("class", "mt-4");
     ask.textContent = questions[questionOrder].prompt;
@@ -242,12 +127,17 @@ function cueQuestion() {
         answersEl.appendChild(choice);
     }
     // Event listeners for answer buttons
-    quizChoiceFirst.addEventListener('click', answerCheck);
-    quizChoiceSecond.addEventListener('click', answerCheck);
-    quizChoiceThird.addEventListener('click', answerCheck);
-    quizChoiceFourth.addEventListener('click', answerCheck);
+    buttonOne = document.querySelector("#button1");
+    buttonTwo = document.querySelector("#button2");
+    buttonThree = document.querySelector("#button3");
+    buttonFour = document.querySelector("#button4");
+    buttonOne.addEventListener('click', answerCheck);
+    buttonTwo.addEventListener('click', answerCheck);
+    buttonThree.addEventListener('click', answerCheck);
+    buttonFour.addEventListener('click', answerCheck);
 };
 
+// Check if the answer is correct/incorrect  
 function answerCheck() {
     if (this.value == question.answer) {
         correctText.setAttribute("id", "correctText.show");
@@ -269,41 +159,87 @@ function answerCheck() {
     }
 };
 
+// Ends game and submits name
+function endGame() {
+    var target = document.querySelector('#playerName');
+    var form = document.createElement('form');
+    var div = document.createElement('div');
+    var label = document.createElement('label');
+    var field = document.createElement('input');
+    var submit = document.createElement('button');
+    label.textContent = "Please enter your name: ";
+    label.setAttribute("class", "mr-2")
+    form.id = "#form";
+    label.for = "player";
+    label.type = "text"
+    submit.id = "submitName";
+    field.id = "player";
+    field.type = "text";
+    field.name = "player";
+    submit.textContent = "Submit";
+    submit.setAttribute("class", "btn-sm btn-success px-4 mt-2");
+    field.setAttribute("class", "form-control");
+    div.setAttribute("class", "form-inline");
+    target.appendChild(form);
+    form.appendChild(div);
+    div.appendChild(label);
+    div.appendChild(field);
+    target.appendChild(submit);
+    submit.addEventListener("click", savePlayerName);
+};
 
+// Saves player's name if highscore is new
+function savePlayerName(event) {
+    event.preventDefault();
+    var playerName = player.value.trim();
+    var user = {
+        name: playerName,
+        score: score,
+    }
+    leaderboard.push(user);
+    localStorage.setItem("leaderboard", JSON.stringify(leaderboard));
+    var target = document.querySelector('#playerName');
+    while (target.firstChild) {
+        target.removeChild(target.firstChild);
+    }
+    createLeaderboard();
+};
 
-// function that will be a click handler for the answer buttons -> accept event as param -> added .quiz-choice-container
-    // grab event.target.textContent save as choice var
-    // if statement -> compare allQuestions[count].correct === choice
-        // run logic for a correct answer
-        // user feedback -> increment score
-    // else
-        // run logic for incorrect answer
-        // user feedback -> decrement score -> time from clock???
-    // if count < allQuestions.length
-        // show the next button
-    // else
-        // endGame()
+// Create leaderboard
+function createLeaderboard() {
+    leaderboard = JSON.parse(localStorage.getItem("leaderboard"));
+    leaderboard.sort((a, b) => b.score - a.score);
+    var leader = document.createElement('ol');
+    leader.setAttribute("class", "text-left");
+    questionEl.appendChild(leader);
+    for (var i = 0; (i < leaderboard.length) && (i <4); i++) {
+        var list = document.createElement('li');
+        list.textContent = leaderboard[i].name + " : " + leaderboard[i].score;
+        list.style = "font-size: .7em";
+        leader.appendChild(list);
+    }
+    // Create "Try Again?" button
+    var play = document.createElement('button');
+    play.id = "play"
+    play.textContent = "Try Again?"
+    playButton = document.querySelector("#tryAgain")
+    playButton.appendChild(play);
+    play.setAttribute("class", "btn btn-primary px-4");
+    play.addEventListener("click", tryAgain);
+};
 
-// next button click handler
-    // increment count
-    // showQuestion()
-
-
-
-// when user presses start
-    // set count = 0
-    // start timer
-    // reveal quiz
-    // check answer
-        // when user presses answer button
-            // if answer correct add time, add to score
-            // else subtract time
-            // count ++
-    // "if count =" statements for questions and answer display
-    // if count > no. of questions
-        // reveal score
-        // local storage score
-        // local storage user provided name
-
-
-
+// Offer user a chance to play again and call functions to reset game to pre-start status
+function tryAgain() {
+    startButtonEl.disabled = false;
+    startButtonEl.setAttribute("class", "btn btn-primary px-4 mt-5");
+    playButton.removeChild(play);
+    var target = document.querySelector('#question');
+    while (target.firstChild) {
+        target.removeChild(target.firstChild);
+    }
+    questionOrder = 0;
+    countTime = 0;
+    score = 0;
+    timerEl.textContent = countTime;
+    returnHighscore();
+};
